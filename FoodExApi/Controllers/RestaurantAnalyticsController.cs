c943ebd894228e8662f2ad43fdc363892244ea06
Mouad <lasri.mouad98@gmail.com>
2020-04-07 16:43:31 +0100

    Added Daily Revenue, Users and Total Revenues Analytics

--- FoodExApi/Controllers/RestaurantAnalyticsController.cs
+++ FoodExApi/Controllers/RestaurantAnalyticsController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using FoodExApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FoodExApi.Controllers
 {
@@ -20,23 +22,77 @@ namespace FoodExApi.Controllers
             this.db = dbContext;
         }
 
-        // GET api/RestaurantsAnalytics/{id}     id = RestaurantId
+        // GET api/RestaurantAnalytics/{id}     id = RestaurantId
         // Get Count of all order categories of the last 24 hours
         [HttpGet("{id}/daily")]
         public ActionResult GetDailyOrders(int restaurantId)
         {
+            var db = this.db;
 
-            int ordersReceivedDaily = db.AppOrder.Where(appOrder => appOrder.RestaurantId == restaurantId).Count();
+            // Getting the daily revenues
+            var dailyRevenuesResults = (from orderDetails in db.OrderDetails
+                           join appOrder in db.AppOrder on orderDetails.OrderId equals appOrder.OrderId
+                           join item in db.Item on orderDetails.ItemId equals item.ItemId
+                           join makeOrder in db.MakeOrder on appOrder.OrderId equals makeOrder.OrderId
+                           where appOrder.RestaurantId == restaurantId
+                           && makeOrder.DateOrdered.Value.Day == 29  // replace by DateTime.Now.Day
+                           && makeOrder.DateOrdered.Value.Month == 3 // replace by DateTime.Now.Month
+                           && makeOrder.DateOrdered.Value.Year == 2020 // replace by DateTime.Now.Year
+                           select new
+                           {
+                               RestaurantId = appOrder.RestaurantId,
+                               Quantity = orderDetails.Quantity,
+                               Price = item.Price
+                           }).ToList();
 
-            var x = new
+            decimal dailyRevenues = 0;
+            foreach (var item in dailyRevenuesResults)
             {
-                orderCompleted = 3,
-                ordersReceived = 5
-            };
+                dailyRevenues += item.Quantity.Value * item.Price.Value;
+            }
 
-            return Ok();
+            // Getting the daily orders received
+            var dailyOrdersReceived = (from makeOrder in db.MakeOrder
+                                       join appOrder in db.AppOrder on makeOrder.OrderId equals appOrder.OrderId
+                                       where appOrder.RestaurantId == restaurantId
+                                       && makeOrder.DateOrdered.Value.Day == 29 // replace by DateTime.Now.Day
+                                       && makeOrder.DateOrdered.Value.Month == 3 // replace by DateTime.Now.Month
+                                       && makeOrder.DateOrdered.Value.Year == 2020 // replace by DateTime.Now.Year
+                                       select appOrder
+                                       ).Count();
+
+            // Getting the unique daily users 
+            var dailyUniqueUsers = (from makeOrder in db.MakeOrder
+                                    join appOrder in db.AppOrder on makeOrder.OrderId equals appOrder.OrderId
+                                    where appOrder.RestaurantId == restaurantId
+                                    && makeOrder.DateOrdered.Value.Day == 29 // replace by DateTime.Now.Day
+                                    && makeOrder.DateOrdered.Value.Month == 3 // replace by DateTime.Now.Month
+                                    && makeOrder.DateOrdered.Value.Year == 2020 // replace by DateTime.Now.Year
+                                    select makeOrder.UserId
+                                    ).Distinct().Count();
+
+            // Getting the total revenues of restaurant
+            var totalRevenuesResults = (from orderDetails in db.OrderDetails
+                                 join appOrder in db.AppOrder on orderDetails.OrderId equals appOrder.OrderId
+                                 join item in db.Item on orderDetails.ItemId equals item.ItemId
+                                 join makeOrder in db.MakeOrder on appOrder.OrderId equals makeOrder.OrderId
+                                 where appOrder.RestaurantId == restaurantId
+                                 select new
+                                 {
+                                     RestaurantId = appOrder.RestaurantId,
+                                     Quantity = orderDetails.Quantity,
+                                     Price = item.Price
+                                 }).ToList();
+
+            decimal totalRevenues = 0;
+            foreach (var item in totalRevenuesResults)
+            {
+                totalRevenues += item.Quantity.Value * item.Price.Value;
+            }
+
+            // Return anonymous type
+            return Ok(new { DailyRevenues = dailyRevenues, DailyOrdersReceived = dailyOrdersReceived, DailyUniqueUsers = dailyUniqueUsers, TotalRevenues = totalRevenues });
+            
         }
-
-
     }
 }
---
+++ FoodExApi/Models/RestaurantAnalytics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodExApi.Models
+{
+    public class RestaurantAnalytics
+    {
+        public float OrdersRevenuesDaily { get; set; }
+    }
+}
